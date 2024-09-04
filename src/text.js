cfcc47ffaa9aaf7388ab8ea0259bf5622c6a7e99
parthblockcube123 <parth.s@blockcube.co>
2024-09-04 17:28:49 +0530

    add : fix commented line

--- src/text.js
+++ src/text.js
@@ -1,8 +1,6 @@
 const toText = (unicodeString) => {
      try {
           let textArray = [];
-          // console.log("wwwwwwwwwwwwwwwwwwwwwwwwww");
-          // let unicodeStr = "0x4d0x790x200x6e0x610x6d0x650x200x690x730x200x700x610x720x740x68";
           let unicodeStr = unicodeString;
           for(let i=0;i<=unicodeStr.length;i+=4){
                let hexToText = String.fromCharCode(parseInt(unicodeStr.substring(i,i+4),16))
